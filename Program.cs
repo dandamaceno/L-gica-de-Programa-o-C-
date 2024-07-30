c7279d99e2de765158d65f5322e07ceb16af0203
Daniel Damaceno <danielpbj@hotmail.com>
2024-07-30 19:08:43 -0300

    Ok

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uri1000
+{
+    class URI
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                Console.WriteLine("Hello World!");
+
+            }
+
+        }
+    }
+}
